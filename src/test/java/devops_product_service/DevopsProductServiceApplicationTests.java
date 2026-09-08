71a26ff174b79ee3290d9ff56ba74e66c2cebbd1
yuktha <yukthamuki@gmail.com>
2026-09-08 20:37:06 +0530

    first commit

---
+++ src/test/java/devops_product_service/DevopsProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package devops_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
